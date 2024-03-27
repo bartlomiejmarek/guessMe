b5ed08a0c766230fd9d13e08abafffca34ee69ad
bartek <bart.marek00@gmail.com>
2024-03-27 23:02:30 +0100

    Langchain adaptation

---
+++ user_game.py
@@ -0,0 +1,65 @@
+from random import choice
+
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_message_histories.in_memory import ChatMessageHistory
+from langchain_community.llms import Ollama
+
+from agents import HostAgent, Mode, GameAgent
+from utils.categories import CATEGORIES
+from utils.prompts import CREATOR_PROMPT, ANSWERER_PROMPT, QUESTIONER_PROMPT
+
+MODE = Mode.HARD
+LLM_ROLE = "Questioner"
+# LLM_ROLE = "Answerer"
+
+# use llm to create a secret password with fixed length and context (category)
+# TODO: try to improve the prompt or try with other models and model's settings (mostly temperature)
+#  to make more predictable output - sometimes the length and level is inadequate and  the phrase is repeated.
+
+password = HostAgent(
+    llm=Ollama(
+        model="llama2",
+        temperature=0.8
+    ),
+    prompt=PromptTemplate(
+        input_variables=["length", "category"],
+        template=CREATOR_PROMPT
+    )
+).conversation.run(length=MODE.value, category=choice(CATEGORIES), level=MODE.name)
+
+gamer = GameAgent(
+    llm=Ollama(
+        model="llama2",
+        # callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+        temperature=0.0
+    ),
+    prompt=PromptTemplate(
+        input_variables=["input", "history"],
+        template=ANSWERER_PROMPT.format(word=password, history='{history}', input='{input}')
+    ),
+
+    memory=ConversationBufferMemory(
+        chat_memory=ChatMessageHistory(),
+        ai_prefix='Answerer',
+        human_prefix="Questioner")
+) if LLM_ROLE.lower() == "answerer" else GameAgent(
+    llm=Ollama(
+        model="llama2",
+        # callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+        temperature=0.0
+    ),
+    prompt=PromptTemplate(
+        input_variables=["input", "history"],
+        template=QUESTIONER_PROMPT),
+    memory=ConversationBufferMemory(
+        chat_memory=ChatMessageHistory(),
+        ai_prefix='Questioner',
+        human_prefix="Answerer")
+)
+
+questioner_input = ""
+while True:
+    ai_response = gamer.play(questioner_input)
+    questioner_input = input('> ')
+    print("AI: {ai_response}")
